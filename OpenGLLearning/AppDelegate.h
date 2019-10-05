c0a1d86695d463c56938f6c1f823b620a7b5f8bb
liqiang01 <liqiang01@inke.cn>
2019-10-05 17:45:03 +0800

    Initial Commit

---
+++ OpenGLLearning/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGLLearning
+//
+//  Created by konglee on 2019/10/5.
+//  Copyright © 2019 konglee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
